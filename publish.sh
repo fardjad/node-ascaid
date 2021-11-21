b4466024efc921fedb4478b0fb4ae7267926e8b5
Fardjad Davari <public@fardjad.com>
2021-11-21 12:40:15 +0100

    Update publish helper script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+VERSION=$(node -e 'import("./lib/utils.js").then(utils => utils.readVersion()).then(version => process.stdout.write(version))')
+DOCKER_IMAGE_NAME="fardjad/ascaid"
+
 cp docs/README.md .
 npm publish
+
+docker build -t "${DOCKER_IMAGE_NAME}:${VERSION}" .
+docker push "${DOCKER_IMAGE_NAME}:${VERSION}"
+
+docker tag "${DOCKER_IMAGE_NAME}:${VERSION}" "${DOCKER_IMAGE_NAME}:latest"
+docker push "${DOCKER_IMAGE_NAME}:latest"
+
+docker pushrm fardjad/ascaid
+
 rm README.md
